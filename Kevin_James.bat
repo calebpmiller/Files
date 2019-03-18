99be9c64c25866a50f028f2e6c10a63910bfcabc
calebpmiller <calebpmiller@users.noreply.github.com>
2019-03-18 17:21:26 -0400

    Update Kevin_James.bat

--- Kevin_James.bat
+++ Kevin_James.bat
@@ -12,38 +12,66 @@
     exit
     
     :M
-      reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "%temp%\1.jpg" /f 
+      reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f 
+      reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "%temp%\1.jpg" /f
+      reg delete "HKCU\Software\Microsoft\Internet Explorer\Desktop\General" /v WallpaperStyle /f
+      reg add "HKCU\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
+      RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters ,1 ,True
       exit
       goto :Yeet 
     
     :Tue
-      reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "%temp%\2.jpg" /f 
+      reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f 
+      reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "%temp%\2.jpg" /f
+      reg delete "HKCU\Software\Microsoft\Internet Explorer\Desktop\General" /v WallpaperStyle /f
+      reg add "HKCU\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
+      RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters ,1 ,True
       exit
       goto :Yeet 
     
     
     :W
-      reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "%temp%\3.jpg" /f 
+      reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f 
+      reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "%temp%\3.jpg" /f
+      reg delete "HKCU\Software\Microsoft\Internet Explorer\Desktop\General" /v WallpaperStyle /f
+      reg add "HKCU\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
+      RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters ,1 ,True
       exit
       goto :Yeet 
     
     :Thu
-      reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "%temp%\4.jpg" /f 
+      reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f 
+      reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "%temp%\4.jpg" /f
+      reg delete "HKCU\Software\Microsoft\Internet Explorer\Desktop\General" /v WallpaperStyle /f
+      reg add "HKCU\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
+      RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters ,1 ,True
       exit
       goto :Yeet 
     
     :F
-      reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "%temp%\5.jpg" /f 
+      reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f 
+      reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "%temp%\5.jpg" /f
+      reg delete "HKCU\Software\Microsoft\Internet Explorer\Desktop\General" /v WallpaperStyle /f
+      reg add "HKCU\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
+      RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters ,1 ,True
       exit
       goto :Yeet 
     
     :Sat
-      reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "%temp%\6.jpg" /f 
+      reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f 
+      reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "%temp%\6.jpg" /f
+      reg delete "HKCU\Software\Microsoft\Internet Explorer\Desktop\General" /v WallpaperStyle /f
+      reg add "HKCU\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
+      RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters ,1 ,True
       exit
       goto :Yeet 
     
     :Sun
-      reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "%temp%\7.jpg" /f 
+      reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f 
+      reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "%temp%\7.jpg" /f
+      reg delete "HKCU\Software\Microsoft\Internet Explorer\Desktop\General" /v WallpaperStyle /f
+      reg add "HKCU\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
+      RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters ,1 ,True
       exit
       goto :Yeet 
       
