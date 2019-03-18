2ed1c33f0424960f7d2e8021d70b3a6f0d8b5cb3
calebpmiller <calebpmiller@users.noreply.github.com>
2019-03-18 15:13:22 -0400

    Update Kevin_James.ps1

--- Kevin_James.ps1
+++ Kevin_James.ps1
@@ -11,10 +11,10 @@
 Start-Process -FilePath %appdata%\Microsoft\Windows\Start Menu\Programs\Startup\Kevin_James.bat
 
 
-@echo off
-reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f 
-reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "C:\[LOCATION OF WALLPAPER HERE]" /f 
-reg delete "HKCU\Software\Microsoft\Internet Explorer\Desktop\General" /v WallpaperStyle /f
-reg add "HKCU\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters ,1 ,True
-exit
+#@echo off
+#reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f 
+#reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "C:\[LOCATION OF WALLPAPER HERE]" /f 
+#reg delete "HKCU\Software\Microsoft\Internet Explorer\Desktop\General" /v WallpaperStyle /f
+#reg add "HKCU\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
+#RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters ,1 ,True
+#exit
